f3bb8da25667db6ae430662965b719afbe763119
Mark R. Ciccarello <bitsinmotion@gmail.com>
2012-11-18 18:54:25 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asqus::Application.config.secret_token = 'bb0fc1bf6eaf2bc8beb1bf775ad58b933e15e94aaf01b134d624cadb514b3cb217b3d66f475a10cf36ae92a3e4c8cd84a279e3d1ed8a90f3ed8bd26c08f03826'
